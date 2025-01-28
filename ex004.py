c81ecdcf6ef64849d5c927ec306f801d5a1d835f
innospace231 <brayanniconiconiii@gmail.com>
2025-01-28 01:45:24 -0300

    Add files via upload

---
+++ ex004.py
@@ -0,0 +1,11 @@
+# 004 Faça um programa que leia algo pelo o teclado e mostre na tela o seu tipo e todas as informações sobre ela.'
+
+# 003 crie um programa que leia dois numeros e mostre a soma entre eles.
+
+n = input('digite aqui:  ')
+
+print(type(n))
+print (n.isnumeric())
+print (n.isspace())
+print(n.isdigit())
+print(n.isdecimal())
